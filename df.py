78fb27766d3ec46a7056df38f39e247f7728fb2c
romanov-lyubomir <romanov.lyubomir+github@protonmail.com>
2022-11-19 18:24:11 +0200

    Leave only DATABASE_URL and remove df.py

--- df.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import random
-
-verb = pd.read_csv('static/data/italian/verbs/abitare.csv').values.tolist()
-print(verb)
--- main.py
+++ main.py
@@ -111,7 +111,7 @@ gravatar = Gravatar(
     app, size=100, rating='g', default='retro',
     force_default=False, force_lower=False, use_ssl=False, base_url=None
 )
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///users.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
